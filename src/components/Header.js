aa09197d3a518812b00611d8a41fac5ce07a5372
hotsoup42 <jose22gw@gmail.com>
2021-10-07 14:50:49 -0700

    header now includes import Nav from ./nav and displays string data but not a tag yet

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
+import Nav from "./Nav"
 export default function Header(){
     return(
         <div>
             <header>
                 <h1>Jose E Diaz</h1>
+                <Nav/>
             </header>
         </div>
     )
---
+++ src/components/Nav.js
@@ -0,0 +1,8 @@
+export default function Nav(){
+    return (
+        <div>
+            <nav><a>Resume</a><a>Contact</a>
+            </nav>
+        </div>
+    )
+}
