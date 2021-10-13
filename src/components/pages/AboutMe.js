d6adf704ac85467fdec1ba555982c27ef73abe19
hotsoup42 <jose22gw@gmail.com>
2021-10-13 14:15:31 -0700

    updated aboutme.js and Contact info.js

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -1,7 +1,8 @@
 export default function AboutMe(){
     return (
         <div>
-            <p>About me</p>
+            <h3>About me</h3>
+            <p>Hi, my name is Jose E. Diaz. I am a full stack web-Developer, currently enrolled in University of Washington's code boot camp. with a background in mechanical repair. enthusiastic to start learning more whether its on the job, at home or enrolled in a course/school.</p>
         </div>
     )
 }
--- src/components/pages/ContactInfo.js
+++ src/components/pages/ContactInfo.js
@@ -1,7 +1,12 @@
 export default function ContactInfo(){
     return(
         <div>
-            <h3>Contact info:1-800-1234</h3>
+            <h3>Contact info</h3>
+            <ul>
+                <li>@gmail.com</li>
+                <li>phone #: 1-800-0000</li>
+                <li>githubUsername:git hub</li>
+            </ul>
         </div>
     )
 }
