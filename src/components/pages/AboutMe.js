e188740a5a9e0c62df3b282232294667ced540f4
hotsoup42 <jose22gw@gmail.com>
2021-10-19 18:33:38 -0700

    changed profile pic in aboutMe and added it to assets folder

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -2,7 +2,7 @@ export default function AboutMe(){
     return (
         <div>
             <h3>About me</h3>
-            <img className="profile-pic" src={`${process.env.PUBLIC_URL}/assets/profilepicture.png`} alt="Jose Diaz"/>
+            <img className="profile-pic" src={`${process.env.PUBLIC_URL}/assets/temp-profile-pic.jpg`} alt="Jose Diaz profilepic"/>
             <p className='ptag'>Hi, my name is Jose E. Diaz. I am a full stack web-Developer, currently enrolled in University of Washington's code boot camp. with a background in mechanical repair. enthusiastic to start learning more whether its on the job, at home or enrolled in a course/school.</p>
         </div>
     )
