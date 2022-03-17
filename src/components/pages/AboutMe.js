1c9473b741c374dc8e514db55b579c420d3f55f8
Hotsoup22 <jose22gw@gmail.com>
2022-03-16 17:19:25 -0700

    changed what p tag said

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -1,25 +1,12 @@
-import Portfolio from '../pages/Portfolio'
-
+// import Portfolio from '../pages/Portfolio'
 import Container from "react-bootstrap/Container";
-
-import  renderPage  from "../PortfolioPage";
+// import  renderPage  from "../PortfolioPage";
 
 export default function AboutMe(){
     return (
         <Container  >
             <img className="profile-pic" src={`${process.env.PUBLIC_URL}/assets/temp-profile-pic.jpg`} alt="Jose Diaz profilepic"/>
-        
-            <p className='aboutMePtag'>  A Full Stack Web-Developer, with a newly found passion for software development. I'm excited to help create the products of tomorrow.</p>
+            <p className='aboutMePtag'>  A Full Stack Web-Developer, with a newly found passion for software development. </p>
         </Container>
     )
 }
-
-
-
-// const aboutMePtag = document.querySelector ('.aboutMePtag')
-//  aboutMePtag.addEventListener("click", function pTagLink(){
-//     renderPage('Portfolio')
-//  })
-//  function handleAboutmeClick(){
-    // style={{color: "red"}}
-//  }
