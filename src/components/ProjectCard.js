6c0fcb820d9d02d224036695cf5f128c79a77ea6
Hotsoup22 <jose22gw@gmail.com>
2022-03-18 17:13:00 -0700

    deleted col, row, and their imports.

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,7 +1,5 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import Card from "react-bootstrap/Card";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+
 import Container from "react-bootstrap/Container";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
 
@@ -17,20 +15,15 @@ export default function ProjectCard({
 }) {
   return (
     <Container className="container">
-      <Row  className="g-4">
-        {Array.from({ length: 1 }).map((_) => (
-          <Col>
-
-            <Card style={{ border: "5px solid darkGrey" }}>
+            <Card >
               <Card.Img 
                 variant="top"
                 src={`${process.env.PUBLIC_URL}/assets/${image}`}
-                href
                 alt="Application Image"
               />
               <Card.Body>
                 <Card.Title className="card-title">{title}</Card.Title>
-                <Card.Text className="card-text">
+
                 <p className="description">Description: {description}</p>  
                 <p className="features">Features: {features}</p>
                 <p className="technologies">Technologies: {technology}</p>
@@ -52,12 +45,9 @@ export default function ProjectCard({
                       GitHub Repository
                     </a>
                   </ButtonGroup>
-                </Card.Text>
               </Card.Body>
             </Card>
-          </Col>
-        ))}
-      </Row>
+
     </Container>
   );
 }
