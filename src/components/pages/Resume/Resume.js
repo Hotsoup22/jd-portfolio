e4a65002bd83e7658210405acd0250f9ba4b56bd
Hotsoup22 <jose22gw@gmail.com>
2022-04-12 15:29:04 -0700

    moved both files to folder to keep css file close, created styles for the resume tab. alot will change backup point.

--- src/components/pages/Resume/Resume.js
+++ src/components/pages/Resume/Resume.js
@@ -1,35 +1,58 @@
 import Card from 'react-bootstrap/Card'
+import Stack from 'react-bootstrap/Stack'
 import './resume.css'
+import profilePic from'../../../assets/temp-profilePic.jpg'
 export default function Resume(){
     return(
         <Card className='resumeDiv'>
-            <Card.Header>
-            <h3>Full-Stack Web Developer</h3>
-            <a href="https://drive.google.com/file/d/122HYGt1mfoV6cdf2rqYJxIgJkrvwPzfq/view?" target="_blank" rel="noreferrer">pdf download</a>
+            <Card.Header className='card-Header-Resume'>
+            <img src={profilePic} alt='pictures goes here' className='profilePic' ></img>
+            <div className='resumeIntroDiv'>
+                <h3 className='full-Stack-Web-Dev'>Full-Stack Web Developer</h3>
+                <p className=' resumePIntro'> Ready to develop databases, create user interfaces, write/test/debug code. Innovative, Motivated, Equipped With A Certificate In Full Stack Web Development, From the University of Washington.  with a background in heavy machinery repair/maintaince, Forever student of computer science.  </p>
+            </div>
             </Card.Header>
-            <div>
-                <h5>Summary</h5>
-                <p className='ptag'>Focused and quick-Learing Developer</p>
-            </div>
-            <div>
-                <h5>Skills</h5>
-                <ul className='ptag'>
-                        <li>HTML5</li>
-						<li>CSS3</li>
-						<li>JavaScript</li>
-						<li>jQuery</li>
-						<li>Handlebars.js</li>
-						<li>React.js</li>
-                        <li>Node.js</li>
-						<li>Express.js</li>
-						<li>MySQL2</li>
-						<li>Sequelize</li>
-						<li>MongoDB</li>
-						<li>Mongoose.js</li>
-                        
-                </ul>
+            <Card.Body>
+                <section>
+                  
+                    <a>jose22gw@gmail.com</a>
+                    <a> phone #</a>
+                    <a>Federal way, WA</a>
+                    <a>portfolio</a>
+                    <a>linkedin</a>
+                    <a>github/Hotsoup22</a>
 
-            </div>
+                </section>
+
+                <section>
+                    <h5>Interest</h5>
+                    <p></p>
+                </section>
+            </Card.Body>
+            <Card.Body>
+                <section>
+                <h4>Work Experience</h4>
+                <p >BinFord</p>
+                <p> Amazon wharehouse</p>
+                </section>
+
+                <section>
+                    <h5> courses and education</h5>
+                    <p>bootcamp</p>
+                    <p>highschool</p>
+                </section>
+
+                <section>
+                    <h5>Technical Skills</h5>
+                    <p></p>
+                </section>
+
+         
+            </Card.Body>
+            {/* <Card.Footer>
+            
+                <p></p>
+            </Card.Footer> */}
         </Card>
     )
 
