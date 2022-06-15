d29268f0b281f1e78080f3414250c6e44a866088
Hotsoup22 <jose22gw@gmail.com>
2022-06-14 22:38:32 -0700

    formatted page, commented out profile pic

--- src/components/pages/AboutMe/AboutMe.js
+++ src/components/pages/AboutMe/AboutMe.js
@@ -11,89 +11,68 @@ export default function AboutMe() {
   const [choosenFact, setChoosenFact] = useState("");
   return (
     <Container>
-      <section className="aboutme-section">
-        <img
+      {/* <img
           src={profilePic}
           alt="pictures goes here"
           className="profilePic"
-        ></img>
-        <p className="aboutMePtag">
+        ></img> */}
+      {/* <p className="aboutMePtag">
           {" "}
           From conception to production with intuitive designs.
-        </p>
+        </p> */}
 
-        {/* random facts about me */}
-        <Card className="factsAboutMeCard" bg="dark" text="white">
-          <Card.Header>
-            <h5>Facts About Me!</h5>
-          </Card.Header>
-          <Card.Body>
-            <textarea
-              id="randomFactTextArea"
-              placeholder="RANDOM Fact.."
-              defaultValue={choosenFact}
-            ></textarea>
-          </Card.Body>
-          <Card.Footer>
-            <button
-              className="btn,randomFactBtn"
-              onClick={() => setChoosenFact(randomFact())}
-            >
-              Push
-            </button>
-          </Card.Footer>
-        </Card>
-      </section>
-      {/* <section>
-                <Card>
-                    <p></p>
-                </Card>
-            </section> */}
+      {/* random facts about me */}
+      <Card className="factsAboutMeCard" bg="dark" text="light">
+        <Card.Header>
+          <h5>About Me</h5>
+        </Card.Header>
+        <Card.Body className="factsAboutMeCardBody">
+          <textarea
+            id="randomFactTextArea"
+            placeholder="RANDOM Fact.."
+            defaultValue={choosenFact}
+          ></textarea>
+        </Card.Body>
 
-      <section className="aboutme-section">
-        <Card bg="dark" className="card-aboutme">
-          <Card.Header className="card-aboutme-Header">
-            <h4 className="honorableMention" style={whiteText}>
-              Honorable Mention:
-            </h4>
-            <h4 style={whiteText} className="pi-Card-Title">
-              RassberryPi4 IR-Sensor
-            </h4>
-            {/* <img href="" alt='pictures goes here'></img> */}
-          </Card.Header>
-          <Card.Body className="card-aboutme-Body">
-            <p>
-              Git-Hub Repo:
-              <a href="https://github.com/Hotsoup22/ir-sensor-rassberryPi4">
-                GitHub rassberryPi4 IR temp sensor.
-              </a>
-            </p>
+        <button
+          className="btn,randomFactBtn"
+          onClick={() => setChoosenFact(randomFact())}
+        >
+          Push
+        </button>
+      </Card>
 
-            {/* <p>I had alot of fun completing the project, more importanly there was plenty of exposure to , python and linux. the project made me feel like I could create and program Wall-E or beomce the next bill gates or steve jobs. </p>  */}
-            <p>
-              I had alot of fun completing the project, more importanly there
-              was exposure to , python, linux and also circuitry. I learned to
-              use a sodder iron for this, bought a bare IR-sensor. A little
-              research and A can do attitude, I had put togther My own
-              contactless Ir-Temp Sensor. while not being the most diffcult
-              thing, it opened my eyes to the potential in software deleopment,
-              and how it can improve the quality of everday life.
-            </p>
-          </Card.Body>
-        </Card>
-      </section>
+      <Card bg="dark" text="light" className="card-aboutme">
+        <Card.Header  className="card-aboutme-Header">
+          <h4 className="honorableMention">Honorable Mention:</h4>
+          <h4 className="pi-Card-Title">RassberryPi4 IR-Sensor</h4>
+          {/* <img href="" alt='pictures goes here'></img> */}
+        </Card.Header>
+        <Card.Body className="card-aboutme-Body">
+            <a href="https://github.com/Hotsoup22/ir-sensor-rassberryPi4">
+              GitHub rassberryPi4 IR temp sensor.
+            </a>
+          {/* <p>I had alot of fun completing the project, more importanly there was plenty of exposure to , python and linux. the project made me feel like I could create and program Wall-E or beomce the next bill gates or steve jobs. </p>  */}
+          <p>
+            I had alot of fun completing the project, more importanly there was
+            exposure to , python, linux and also circuitry. I learned to use a
+            sodder iron for this, bought a bare IR-sensor. A little research and
+            A can do attitude, I had put togther My own contactless Ir-Temp
+            Sensor. while not being the most diffcult thing, it opened my eyes
+            to the potential in software deleopment, and how it can improve the
+            quality of everday life.
+          </p>
+        </Card.Body>
+      </Card>
 
-      <section className="aboutme-section"> 
-        {/* <Card bg='dark' className="card-aboutme "> */}
-        <p className="info">
-          please checkout my Git Hub Repo. Im always working on a new projects,
-          or updating/adding features to old repo's.
-        </p>
-        <a href="https://github.com/Hotsoup22">GitHub</a>
-        <br></br>
-        <a href="https://www.linkedin.com/in/jose-diaz-07043220b/">Linked-In</a>
-        {/* </Card> */}
-      </section>
+      <Card bg='dark' text='light' className="card-aboutme ">
+      <p className="info">
+        Checkout my Git Hub Repo. Im always working on a new projects, or
+        updating/adding features to old repo's.
+      </p>
+      <button><a href="https://github.com/Hotsoup22">GitHub</a></button>
+      <button><a href="https://www.linkedin.com/in/jose-diaz-07043220b/">Linked-In</a></button>
+      </Card>
     </Container>
   );
 }
@@ -115,10 +94,10 @@ const randomFact = () => {
   console.log("random Fact function start");
   const randomFactsArray = [
     "Ability to work in a fast-paced and diverse team environment",
-    "Used Computers Since elemntary school, on windows XP!",
-    "self taught how to sodder, and MIG Weld ",
-    "example 4 ",
-    "example 5",
+    "Have owned computers Since elemntary school, using windows XP. Have built 3 since!",
+    "Self-taught solder, and Mig Welding",
+    "Have been called charismatic ",
+    "Natural tinker",
   ];
   // const randomFactsArray = {
   //     1:"Ability to work in a fast-paced and diverse team environment",
